ca9f5c0f4b3d33c4db057dd52efa4b6ff261904f
Yo Lulkin <maiz@lulk.in>
2022-12-12 14:09:46 +0100

    Deploy

--- pages/components/PopUp.tsx
+++ pages/components/PopUp.tsx
@@ -1,13 +1,5 @@
 import Script from "next/script";
 
 export default function PopUp() {
-  return (
-    <>
-      <Script id="mcjs">
-        {`
-    !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/51f10df1955a211120bfeab13/8884199433175b354e2bb626d.js");
-    `}
-      </Script>
-    </>
-  );
+  return <></>;
 }
