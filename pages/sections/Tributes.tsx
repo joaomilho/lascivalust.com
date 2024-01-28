1accb53f7accbf3114d8ea5db7f9eb211ec45041
Yo Lulkin <maiz@lulk.in>
2024-01-28 19:29:24 +0100

    Deploy

--- pages/sections/Tributes.tsx
+++ pages/sections/Tributes.tsx
@@ -3,21 +3,19 @@ import styles from "./About.module.css";
 
 export default function Tributes() {
   return (
-    <>
-      <div className={styles.about}>
-        <div className={styles.sessions}>
-          <Header text="Tributes" />
+    <div className={styles.about}>
+      <div className={styles.sessions}>
+        <Header text="Tributes" />
 
-          <p>
-            For tributes, use My{" "}
-            <a href="https://paypal.me/MLcreativeart">PayPal</a>.
-            <br />
-            Send me a gift on{" "}
-            <a href="https://www.luxylist.it/lascivalust">LuxyList</a> or{" "}
-            <a href="https://www.wishtender.com/lascivalust">Wishtender</a>.
-          </p>
-        </div>
+        <p>
+          For tributes, use My{" "}
+          <a href="https://paypal.me/MLcreativeart">PayPal</a>.
+          <br />
+          Send me a gift on{" "}
+          <a href="https://www.luxylist.it/lascivalust">LuxyList</a> or{" "}
+          <a href="https://www.wishtender.com/lascivalust">Wishtender</a>.
+        </p>
       </div>
-    </>
+    </div>
   );
 }
